a4000363bcff51a0572271258eec0de8269842e3
Maouusama <Tidustidy@gmail.com>
2016-12-16 03:50:10 +0800

    Initial on Detailed

---
+++ app/src/main/java/com/training/android/prelimexam/DetailedTask.java
@@ -0,0 +1,13 @@
+package com.training.android.prelimexam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_task);
+    }
+}
